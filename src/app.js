5427f860240d259d64918deb20d2aad3c1676623
marinouceda <marianouceda@hotmail.com>
2022-03-15 21:59:31 -0300

    primer commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require ('path');
+const port = 3030;
+
+const publicPath = path.join(__dirname, '../', 'public');
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => console.log(`Servidor en línea en puerto ${port}`));
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
