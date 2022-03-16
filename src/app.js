752da4d53bba58cb078b93449392ac335f8247ab
marinouceda <marianouceda@hotmail.com>
2022-03-15 22:26:16 -0300

    agregamos PORT

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require ('path');
-const port = 3030;
+const PORT = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, '../', 'public');
 const app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => console.log(`Servidor en línea en puerto ${port}`));
+app.listen(PORT, () => console.log(`Servidor en línea en puerto ${port}`));
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
